d7420c3d5b70292d562faa9ed1d0474726c3e3bf
vivvoweb <hestv.tv@gmail.com>
2024-10-01 03:20:58 +0100

    Chargement du style.css
     Chargement du style.css

--- functions.php
+++ functions.php
@@ -3,6 +3,6 @@
 // Action  scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles(){
- 
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css'); 
 
  }
